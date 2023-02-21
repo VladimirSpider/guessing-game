e8c05fb6ecd772f7464b273db942717dc375accc
Vladimir <vladimir.bur@overdrive.sg>
2023-02-21 12:36:43 +0300

    created GuessingGame;

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+        this.ourGuess;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        if (this.min === 0) this.ourGuess = Math.round((this.max - this.min) / 2);
+        if (this.min !== 0) this.ourGuess = this.min + Math.round((this.max - this.min) / 2);
+        return this.ourGuess;
     }
 
     lower() {
-
+        this.max = this.ourGuess;
     }
 
     greater() {
-
+        this.min = this.ourGuess;
     }
 }
 
